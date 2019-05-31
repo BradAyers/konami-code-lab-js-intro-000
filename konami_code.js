1f4f65c6527672b4d6cf9a1edaecdecad897b032
Brad Ayers <brad@bradayers.com>
2019-05-31 04:30:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,30 +10,32 @@ const codes = [
   "b",
   "a"
 ];
+function init () {
+  const input = document.querySelector('input')
+  console.log('init');
+  input.addEventListener('keydown', onKeyDownHandler(event));
+};
 
 // Keys for A, B, and C keys.
 const alphabet = ['a', 'b', 'c'];
-
+ 
 // Keep track of index outside of the event handler.
 let index = 0;
-
+ 
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
-
+ 
   if (key === alphabet[index]) {
     index++;
-
+    console.log(key);
+ 
     if (index === alphabet.length) {
       alert("Hurray!");
-
+ 
       index = 0;
     }
   } else {
     index = 0;
   }
 }
-
-const input = document.querySelector('input')
-
-input.addEventListener('keydown', onKeyDownHandler(event));
